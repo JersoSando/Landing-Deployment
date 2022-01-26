7cf43829c79c26c09570cbbeea15894e1f674a03
Jerso Sando <jerso.sando@gmail.com>
2022-01-26 14:48:40 -0700

    file change

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || process.env.SERVER_PORT
 const app = express ()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
+    res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 app.use(express.json())
